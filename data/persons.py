dd7824035c8cbf8cca010f81fd73b6ce141d93b1
Zimbleik <75900046+Zimbleik@users.noreply.github.com>
2023-08-05 16:39:27 +0300

    Delete data directory

--- data/persons.py
+++
@@ -1,5 +0,0 @@
-class Persons:
-    PERSON_1 = {'name': 'Алиса', 'lastname': 'Максимова', 'address': 'Ленина ул. 123',
-                'station': 'Люблино', 'number': '70000000000'}
-    PERSON_2 = {'name': 'Егор', 'lastname': 'Лапшин', 'address': 'Попова ул. 99',
-                'station': 'Аэропорт', 'number': '79999999999'}
--- data/urls.py
+++
@@ -1,4 +0,0 @@
-class DataUrls:
-    DZEN_URL = 'https://dzen.ru/?yredirect=true'
-    SCOOTER_URL = 'https://qa-scooter.praktikum-services.ru/'
-    ORDER_SCOOTER_URL = 'https://qa-scooter.praktikum-services.ru/order'
