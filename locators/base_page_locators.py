f80e7df194c2a54457499e274186bb41d866fc70
Zimbleik <75900046+Zimbleik@users.noreply.github.com>
2023-08-05 16:39:38 +0300

    Delete locators directory

--- locators/base_page_locators.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class BasePageLocators:
-    YANDEX_LOGO = By.XPATH, "//*[@alt='Yandex']"  # Логотип "Яндекс"
-    SCOOTER_LOGO = By.XPATH, "//*[@alt='Scooter']"  # Логотип "Самокат"
